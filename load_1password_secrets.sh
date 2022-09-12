1f9e7da0e80b06ec28a780be5cddb2e738f3f575
Francisco Galarza <franciscogalarza@gmail.com>
2022-09-12 22:26:40 +1000

    Instructions to load 1password key and token

---
+++ load_1password_secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fetch the core pipeline credentials
+op read "op://personal/Core Pipeline Credentials File/1password-credentials.json" \
+    | base64 \
+    | tr '/+' '_-' \
+    | tr -d '=' \
+    | tr -d '\n' > 1password-credentials.json
+kubectl create secret generic op-credentials --from-file=1password-credentials.json -n one-password
+rm 1password-credentials.json
+
+# Fetch the token
+OP_CONNECT_TOKEN=$(op read "op://personal/core pipeline access token - test/credential")
+kubectl create secret generic onepassword-token --from-literal=token=$OP_CONNECT_TOKEN  -n one-password
